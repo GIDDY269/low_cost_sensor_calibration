4c7ed3b426b6c2560e7e8a3405f25e3d24a98eac
GIDDY269 <oviemunooboro@gmail.com>
2023-04-15 10:17:10 -0700

    files

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,7 @@
-import pandas 
-import os
 import sys
+sys.path.append('C:/Users/user/low_cost_sensor_calibration')
+import os
+import pandas as pd
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
@@ -9,7 +10,7 @@ from src.exception import CustomException
 
 @dataclass
 class DATA_INGESTION_CONFIG :
-    artifact_filepath = 'user/low_cost_sensor_calibration/artifacts'
+    artifact_filepath = 'C:/Users/user/low_cost_sensor_calibration/artifacts/'
     data_file_path = os.path.join(artifact_filepath,'ingested_data.csv')
     road_file_path = os.path.join(artifact_filepath,'road_length.csv')
     
@@ -21,7 +22,26 @@ class DATA_INGESTION :
         logging.info('initiating data ingestion')
         
         try:
-            data = pd.read_csv('user/low_cost_sensor_calibration/notebook/data/raw_collocated_data.csv')
-            road_length_df = pd.csv('user/low_cost_sensor_calibration/notebook/data/road_lengths.csv')
+            data = pd.read_csv('C:/Users/user/low_cost_sensor_calibration/notebook/data/raw_collocated_data.csv')
+            road_length_df = pd.read_csv('C:/Users/user/low_cost_sensor_calibration/notebook/data/road_lengths.csv')
             
             logging.info('loaded the data')
+            
+            os.makedirs(self.ingestion_config.artifact_filepath, exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.data_file_path), exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.road_file_path), exist_ok=True)
+            
+            data.to_csv(self.ingestion_config.data_file_path,header=True,index=False)
+            road_length_df.to_csv(self.ingestion_config.road_file_path,header=True,index=False)
+            
+            logging.info('Ingestion complete')
+            
+            return(self.ingestion_config.data_file_path,self.ingestion_config.road_file_path)
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+            
+if __name__=='__main__':
+    obj = DATA_INGESTION()
+    d,r = obj.initiate_data_ingestion()            
