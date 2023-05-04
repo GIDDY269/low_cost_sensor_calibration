ad54c88cd326f3fb8e7515b1fcb1346439edca54
GIDDY269 <oviemunooboro@gmail.com>
2023-05-04 14:20:20 -0700

    files

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ from src.pipeline.predict_pipeline import calibrator
 from sensor_parameter import sensor_parameters
 from datetime import datetime
 import math
+from utils import load_object
+from src.logger import logging
 
 
 
 
 #create app object
 app = FastAPI()
+model = load_object('C:/Users/user/low_cost_sensor_calibration/artifacts/MODEL.pkl')
 
 # index route ,opens up automatically on http://127.0.0.1:8000
 @app.get('/')
@@ -19,11 +22,12 @@ def index():
     return{'WELCOME'}
 
 @app.post('/calibrate')
-def sensor_calibration_endpoint(sensor_data : sensor_parameters):
+def sensor_calibration_endpoint(sensor_data:sensor_parameters):
+    logging.info(f"Received sensor data: {sensor_data}")
     sensor_data = sensor_data.dict()
     cal = calibrator()
     prediction = cal.calibrate(sensor_data)
-    return {'calibrated pm2.5 readings':prediction}
+    return {'Calibrated pm 2.5 reading': str(prediction)}
     
 if __name__ == '__main__':
     uvicorn.run(app,host='127.0.0.1',port=8000)
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -8,35 +8,45 @@ import math
 from utils import load_object
 from datetime import datetime
 
+model = load_object('C:/Users/user/low_cost_sensor_calibration/artifacts/MODEL.pkl')
+
 class calibrator:
     def __init__(self):
         pass
     
-    def calibrate (self,data):
+    def calibrate (self,sensor_data):
         try :
-            logging.info('calibrating sensor readings')
-            pm_cs = data['pm_cs']
-            temp = data['temp']
-            a_road_500 = data['a_road_500']
-            date = datetime.strptime(data['datetime'],format= "%Y-%m-%d %H:%M:%S" )
-            data['Time'] = date.dt.hour
-            data['month'] = date.dt.month
+            logging.info('creating features')
+            pm_cs = sensor_data['pm_cs']
+            temp = sensor_data['temp']
+            humidity = sensor_data['humidity']
+            a_road_500 = sensor_data['a_road_500']
+            date = sensor_data['datetime']
+            logging.info('created datetime')
+            sensor_data['Time'] = sensor_data['datetime'].hour
+            sensor_data['month'] = sensor_data['datetime'].month
+            logging.info('created time and month features')
+            sensor_data['sin_time'] = math.sin(sensor_data['Time'])*2*(math.pi/24)
+            sin_time = sensor_data['sin_time']
+            sensor_data['cos_time'] = math.cos(sensor_data['Time'])*2*(math.pi/24)
+            cos_time = sensor_data['cos_time']
             
-            data['sin_time'] = data['Time'].apply(lambda x : math.sin(x)*2*(math.pi/24))
-            sin_time = data['sin_time']
-            data['cos_time'] = data['Time'].apply(lambda x : math.cos(x)*2*(math.pi/24))
-            cos_time = data['cos_time']
+            sensor_data['sin_month'] = math.sin(sensor_data['month'])*2*(math.pi/12)
+            sin_month = sensor_data['sin_month']
+            sensor_data['cos_month'] = math.cos(sensor_data['month'])*2*(math.pi/12)
+            cos_month = sensor_data['cos_month']
             
-            data['sin_month'] = data['month'].apply(lambda x : math.sin(x)*2*(math.pi/12))
-            sin_month = data['sin_month']
-            data['cos_month'] = data['month'].apply(lambda x : math.cos(x)*2*(math.pi/12))
-            cos_month = data['cos_month']
+            logging.info('created seasonal features')
             
-            del data['datetime','Time','month']
-            #load model
-            model = load_object('C:/Users/user/low_cost_sensor_calibration/artifacts/MODEL.pkl')
-            prediction = model.predict([[pm_cs,temp,a_road_500,sin_time,cos_time,sin_month,cos_month]])
-            logging.info('calibrated readings')
+            del sensor_data['datetime'], sensor_data['Time'], sensor_data['month']
+            
+            logging.info(f'dictionary keys : {sensor_data.keys()}')
+            
+            
+            logging.info(f"Loaded model {model}")
+            prediction = model.predict([[pm_cs,temp,humidity,a_road_500,sin_time,cos_time,sin_month,cos_month]])
+            logging.info(f"Prediction: {prediction}")
+            
             return prediction 
         except Exception as e:
             raise CustomException(e,sys)
